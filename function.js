1ac53e305f4be82f7368ec4bfa625e55b8169423
Isti Olah <iolah2@gmail.com>
2022-09-15 18:34:16 +0200

    Save modify

--- function.js
+++ function.js
@@ -105,6 +105,10 @@ function clearCalender() {
  clickCounter = 0;
 }
 
+function testClick(){
+      $("#selectedDay").add
+}
+
 Date.prototype.addDays = function(days) {
        var date = new Date(this.valueOf());
        date.setDate(date.getDate() + days);
